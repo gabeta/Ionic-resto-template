30a8783e492a413a3c0990091a5c45de449ef46a
Gabeta Soro <gabeta.soro@gmail.com>
2016-07-24 14:11:04 +0000

    Create a LoginCtrl

--- template/www/js/controller/login.js
+++ template/www/js/controller/login.js
@@ -1,3 +1,12 @@
 /**
  * Created by Gabeta on 24/07/2016.
  */
+app.controller('loginctrl',function($scope){
+
+  $scope.slideMenu = false;
+
+  /*$scope.login = function(){
+    $state.go();
+  }*/
+
+})
